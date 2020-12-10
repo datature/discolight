448de5073e8a38c992e1792854a95c84841117ba
Ian Duncan <65128622+ian-datature@users.noreply.github.com>
2020-12-10 14:43:06 +0800

    updated snapshot tests to be a little more flexible (#22)

--- tests/snapshots/test_run_snapshots.py
+++ tests/snapshots/test_run_snapshots.py
@@ -1,9 +1,13 @@
 import os
 import shutil
 import random
-from filecmp import dircmp
 import pytest
+import numpy as np
 from discolight.run import main
+
+from discolight.loaders.image.directory import Directory
+from discolight.loaders.annotation.fourcornerscsv import FourCornersCSV
+from discolight.annotations import annotations_to_numpy_array
 
 
 @pytest.mark.usefixtures("sample_query")
@@ -25,8 +29,33 @@ def test_run(snapshot, sample_query, tmp_path):
         shutil.copytree(tmp_path, "./snapshots/run")
         return
 
-    cmp_result = dircmp(tmp_path, "./snapshots/run")
+    with FourCornersCSV(annotations_file="./snapshots/run/aug_annotations.csv",
+                        normalized=True) as annotation_loader, Directory(
+                            directory="./snapshots/run") as image_loader:
+        snapshot_images = annotation_loader.load_annotated_images(image_loader)
 
-    assert len(cmp_result.left_only) == 0
-    assert len(cmp_result.right_only) == 0
-    assert len(cmp_result.diff_files) == 0
+    with FourCornersCSV(annotations_file=os.path.join(tmp_path,
+                                                      "aug_annotations.csv"),
+                        normalized=True) as annotation_loader, Directory(
+                            directory=tmp_path) as image_loader:
+        run_images = annotation_loader.load_annotated_images(image_loader)
+
+    for image_name in snapshot_images:
+
+        snapshot_image, snapshot_annot = snapshot_images[image_name]
+        run_image, run_annot = run_images[image_name]
+
+        snapshot_bboxes = annotations_to_numpy_array(snapshot_annot)
+        run_bboxes = annotations_to_numpy_array(run_annot)
+
+        assert np.allclose(
+            snapshot_image,
+            run_image), "Image for {} differs".format(image_name)
+        assert np.allclose(
+            snapshot_bboxes,
+            run_bboxes), "Bounding boxes for {} differ".format(image_name)
+
+    for image_name in run_images:
+
+        if image_name not in snapshot_images:
+            assert False, "Image {} not in snapshot".format(image_name)
